e418eff29065715e897e4ebc13a17c870e0ea53d
Waleed Azam <Waleed.ntnu@gmail.com>
2022-09-09 13:07:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_CS_MS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here in this code, we going to have a brief introduction to C# Methods");
+            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("Methods, having a return type, and we need to do some functionality here");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("You entered\t{0}", name);
+
+        }
+    }
+}
